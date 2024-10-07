cf40e383c686682a0262c45edfbe2cf2a0c72ef4
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-07 08:38:40 +0600

    fix: fix some issue in login page

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,104 +1,102 @@
-import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import useAuth from "../../hooks/useAuth";
 import SocialLogin from "../../components/socialLogin/SocialLogin";
 import Swal from "sweetalert2";
-import img from '../../assets/Images/home-page/sign-up-page.jpg'
+import img from "../../assets/Images/home-page/sign-up-page.jpg";
 import { toast } from "react-toastify";
 
-
 const SignIn = () => {
-    const [disabled, setDisabled] = useState(true)
-    const { loginUser } = useAuth()
-    const navigate = useNavigate()
-    const location = useLocation()
-    const from = location.state?.from?.pathname || "/";
-    // console.log("user login for items",location.state);
+  const { loginUser } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
-    useEffect(() => {
-        loadCaptchaEnginge(6);
-    }, [])
-
-    const handleLogin = (e) => {
-        e.preventDefault()
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        const userInfo = { email, password }
-        // console.log(userInfo);
-        loginUser(email, password)
-            .then((result) => {
-                // console.log(result.user);
-                if(result.user){
-                    Swal.fire({
-                        title: "Good job!",
-                        text: "login successfully!",
-                        icon: "success"
-                    });
-                }
-                navigate(from, { replace: true })
-            })
-            .catch(error => {
-                const errorMessage = error.message;
-                toast.error(errorMessage)
-            })
-    }
-
-    const handleValidCaptcha = (e) => {
-        const data = e.target.value;
-        if (validateCaptcha(data)) {
-            setDisabled(false)
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const userInfo = { email, password };
+    // console.log(userInfo);
+    loginUser(email, password)
+      .then((result) => {
+        // console.log(result.user);
+        if (result.user) {
+          Swal.fire({
+            title: "Good job!",
+            text: "login successfully!",
+            icon: "success",
+          });
         }
-        else {
-            setDisabled(true)
-        }
-    }
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+      });
+  };
 
-
-    return (
-        <div className="hero min-h-screen ">
-            <div className="hero-content flex-col lg:flex-row gap-0">
-                <div className="card md:w-1/2 w-full max-w-sm">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <LoadCanvasTemplate />
-                            </label>
-                            <input onBlur={handleValidCaptcha} type="text" name="captcha" placeholder="Type your right captcha" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control mt-6">
-                            {/* TODO: make disabled true */}
-                            <input disabled={false} className="bg-[#EC4F47] p-2 text-white rounded-md " type="submit" value="Login" />
-                        </div>
-                        <small>New here <Link className="text-base font-medium text-red-700/70" to={'/sign-up'}>Create an account</Link></small>
-                    </form>
-                    <div className="divider px-2"></div>
-                    <div className="flex justify-center">
-                        <SocialLogin></SocialLogin>
-                    </div>
-                </div>
-                <div className="text-center w-1/2 lg:text-left">
-                    <img className="h-[610px]" src={img} alt="" />
-                </div>
-            </div>
+  return (
+    <div className="md:w-[30%] w-[90%] mx-auto border border-[#ce3d61]/40 md:my-24 my-10">
+      <form onSubmit={handleLogin} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="p-2 border border-black/30 outline-none"
+            required
+          />
         </div>
-    );
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            className="p-2 border border-black/30 outline-none"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+
+        <div className="form-control">
+          {/* TODO: make disabled true */}
+          <input
+            disabled={false}
+            className="bg-[#991747] p-2 text-white rounded-md "
+            type="submit"
+            value="Login"
+          />
+        </div>
+        <p className="text-center">
+          New here{" "}
+          <Link
+            className="text-base font-medium text-[#991747]"
+            to={"/sign-up"}
+          >
+            Create an account
+          </Link>
+        </p>
+
+        <hr  className="mt-2 mb-3"/>
+
+        {/* social section */}
+        <div className="flex justify-center">
+            <SocialLogin></SocialLogin>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
